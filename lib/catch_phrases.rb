10cb617202ed1abdbcfe39a6839ac1225c568896
Austin Kohler <austinckohler@gmail.com>
2020-03-24 19:43:26 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,23 +1,44 @@
+# def mario
+#   phrase = "It's-a me, Mario!"
+#   puts phrase
+# end
+# mario 
+
+# def toadstool
+#   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+#   puts status
+# end
+
+# toadstool
+
+# def link
+#   phrase = "It's Dangerous To Go Alone! Take This."
+#   puts phrase
+# end
+# link
+
+# def any_phrase(phrase)
+#   puts phrase
+# end
+# any_phrase("Do A Barrel Roll!\n")
+
+
 def mario
   phrase = "It's-a me, Mario!"
   puts phrase
 end
-mario 
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
 
-toadstool
-
 def link
-  phrase = "It's Dangerous To Go Alone! Take This."
-  puts phrase
+  puts "It's Dangerous To Go Alone! Take This."
 end
-link
 
-def any_phrase(phrase)
-  puts phrase
+def all_phrases
+  mario
+  toadstool
+  link
 end
-any_phrase("Do A Barrel Roll!\n")
